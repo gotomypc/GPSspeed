5e0a76f397d912a7a16f113c60f4e6d51224dcce
Karthik Paithankar <karthik.berlin@yahoo.com>
2012-06-22 17:07:10 +0200

    new file:   apks-release/gpsspeed.apk

--- gen/com/biophysics/android/gpsspeed/R.java
+++ gen/com/biophysics/android/gpsspeed/R.java
@@ -21,8 +21,8 @@ public final class R {
         public static final int layoutSign=0x7f070002;
         public static final int tvAccuracy=0x7f070004;
         public static final int tvAltitude=0x7f070003;
-        public static final int tvGpsLat=0x7f070005;
-        public static final int tvGpsLon=0x7f070006;
+        public static final int tvGpsLat=0x7f070006;
+        public static final int tvGpsLon=0x7f070005;
         public static final int tvSpeed=0x7f070001;
     }
     public static final class layout {
